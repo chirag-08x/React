d6231346b8d4353fae94acc2ca27420d67a113fe
chirag-08x <cagrawal081200@gmail.com>
2022-04-22 03:02:49 +0530

    slider started

--- 03-Basic-Projects/basic-projects/07-slider/src/slider/person.jsx
+++ 03-Basic-Projects/basic-projects/07-slider/src/slider/person.jsx
@@ -1,8 +1,9 @@
 import { FaQuoteRight } from "react-icons/fa";
+import styled from "styled-components";
 
 const Person = ({ image, name, title, quote }) => {
   return (
-    <article>
+    <Wrapper>
       <div className="img-container">
         <img src={image} alt={name} />
       </div>
@@ -16,9 +17,42 @@ const Person = ({ image, name, title, quote }) => {
         <p>{quote}</p>
       </div>
 
-      <FaQuoteRight />
-    </article>
+      <FaQuoteRight className="quote" />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.article`
+  display: grid;
+  place-items: center;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  .img-container {
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+
+    img {
+      border-radius: 50%;
+    }
+  }
+
+  .info {
+    text-align: center;
+  }
+
+  .subtitle {
+    text-align: center;
+  }
+
+  .quote {
+    font-size: 2.5rem;
+  }
+`;
 
 export default Person;
--- 03-Basic-Projects/basic-projects/07-slider/src/slider/slider.jsx
+++ 03-Basic-Projects/basic-projects/07-slider/src/slider/slider.jsx
@@ -1,10 +1,16 @@
 import people from "./data";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import Person from "./person";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const Slider = () => {
   const [data, setData] = useState(people);
+  const refContainer = useRef("");
+
+  useEffect(() => {
+    const children = [...refContainer.current.children];
+  }, []);
 
   return (
     <Wrapper>
@@ -12,15 +18,42 @@ const Slider = () => {
         <span>/</span> reviews
       </h1>
 
-      <section className="slider">
+      <section className="slider" ref={refContainer}>
         {data.map((item) => {
           return <Person key={item.id} {...item} />;
         })}
       </section>
+
+      <div className="btns">
+        <button className="btn">
+          <FaAngleLeft />
+        </button>
+
+        <button className="btn">
+          <FaAngleRight />
+        </button>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.main``;
+const Wrapper = styled.main`
+  display: grid;
+  place-items: center;
+  h1 {
+    font-size: 2.5rem;
+    text-align: center;
+    margin: 4rem 0 2rem 0;
+  }
+
+  .slider {
+    width: 90vw;
+    max-width: 800px;
+    height: 450px;
+    margin: 0 auto;
+    /* display: flex; */
+    position: relative;
+  }
+`;
 
 export default Slider;
