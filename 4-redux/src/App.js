48463ecf99e9bca6b26c59c1c05706d9de911191
chirag-08x <cagrawal081200@gmail.com>
2022-06-04 17:11:33 +0530

    modal added

--- 4-redux/src/App.js
+++ 4-redux/src/App.js
@@ -3,17 +3,21 @@ import CartContainer from "./components/cartContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+import Modal from "./components/modal";
 
 const App = () => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-next-line
   }, [cartItems]);
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- 4-redux/src/store.jsx
+++ 4-redux/src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 // This is our whole state. A container for our whole state.
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
